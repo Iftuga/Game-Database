9ef4f28794d6c35df898ad4e5e5a662dd800691a
vvlad <volkov99vlad@gmail.com>
2018-06-02 22:32:56 +0300

    Add scroll

--- display.py
+++ display.py
@@ -1,10 +1,59 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""
+
+        buttons = []
+        for i in range(10):
+            buttons.append(Button(self.frame.interior, text="Button " + str(i)))
+            buttons[-1].pack()
+
+
+"""
+
 def display():
     # first part
     import main
     import tkinter as tk
+    class scrollFrame(tk.Frame):
+        def __init__(self, parent, *args, **kw):
+            tk.Frame.__init__(self, parent, *args, **kw)            
+
+            # create a canvas object and a vertical scrollbar for scrolling it
+            vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
+            vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
+            #vscrollbar.place( x = 725, y = 75, whi)
+            canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                            yscrollcommand=vscrollbar.set)
+            canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+            vscrollbar.config(command=canvas.yview)
+
+            # reset the view
+            canvas.xview_moveto(0)
+            canvas.yview_moveto(0)
+
+            # create a frame inside the canvas which will be scrolled with it
+            self.interior = interior = tk.Frame(canvas)
+            interior_id = canvas.create_window(0, 0, window=interior,
+                                               anchor=tk.NW)
+
+            # track changes to the canvas and frame width and sync them,
+            # also updating the scrollbar
+            def _configure_interior(event):
+                # update the scrollbars to match the size of the inner frame
+                size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+                canvas.config(scrollregion="0 0 %s %s" % size)
+                if interior.winfo_reqwidth() != canvas.winfo_width():
+                    # update the canvas's width to fit the inner frame
+                    canvas.config(width=interior.winfo_reqwidth())
+            interior.bind('<Configure>', _configure_interior)
+
+            def _configure_canvas(event):
+                if interior.winfo_reqwidth() != canvas.winfo_width():
+                    # update the inner frame's width to fill the canvas
+                    canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+            canvas.bind('<Configure>', _configure_canvas)
+
     class MainWindow:
         def __init__(self):
             self.flagSort = 0
@@ -17,71 +66,71 @@ def display():
             self.pD = []
             self.pPu = []
             self.pPr = []
+            self.scrollF = scrollFrame(root)
 
 #Поле = {"Название игры":0, "Жанр":1, "Платформа":2, "Год выпуска":3, "Цена":4, "Разработчик":5, "Издатель":6}
             self.posNameGame = tk.Button( root, text = "Название игры", bg = "white", fg="black")
             i = 0
             self.exit = tk.Button( root, text = "Exit", command = root.destroy, bg = "white", fg="black")
             while ( i < len(self.base)):
-                self.pNG.append(tk.Entry(root))
+                self.pNG.append(tk.Entry(self.scrollF.interior, width = 15  ))
                 i = i + 1
             self.posPlat = tk.Button( root, text = "Платформа", bg = "white", fg="black")
             i = 0
             while ( i < len(self.base)):
-                self.pP.append(tk.Entry(root))
+                self.pP.append(tk.Entry(self.scrollF.interior, width = 12 ))
                 i = i + 1
             self.posGenre = tk.Button( root, text = "Жанр", bg = "white", fg="black")
             i = 0
             while ( i < len(self.base)):
-                self.pG.append(tk.Entry(root))
+                self.pG.append(tk.Entry(self.scrollF.interior, width = 10 ))
                 i = i + 1
             self.posYear = tk.Button( root, text = "Год выпуска", bg = "white", fg="black")
             i = 0
             while ( i < len(self.base)):
-                self.pY.append(tk.Entry(root))
+                self.pY.append(tk.Entry(self.scrollF.interior, width = 12 ))
                 i = i + 1
             self.posDevel = tk.Button( root, text = "Разработчик", bg = "white", fg="black")
             i = 0
             while ( i < len(self.base)):
-                self.pD.append(tk.Entry(root))
+                self.pD.append(tk.Entry(self.scrollF.interior, width = 19 ))
                 i = i + 1
             self.posPublisher = tk.Button( root, text = "Издатель", bg = "white", fg="black")
             i = 0
             while ( i < len(self.base)):
-                self.pPu.append(tk.Entry(root))
+                self.pPu.append(tk.Entry(self.scrollF.interior, width = 19 ))
                 i = i + 1
             self.posPrice = tk.Button( root, text = "Цена", bg = "white", fg="black")
             i = 0
             while ( i < len(self.base)):
-                self.pPr.append(tk.Entry(root))
+                self.pPr.append(tk.Entry(self.scrollF.interior, width = 5 ))
                 i = i + 1
-            self.frame = tk.Frame(root)
             self.init_widget()
         def init_widget(self):
-            #self.frame.place( x = 100, y = 50, width = 750, height = (len(self.base) + 1) * 25)
             self.posNameGame.bind('<ButtonRelease-1>',
                          lambda event: self.sortDisp(event, "Название игры"))
-            self.posNameGame.place(x = 100, y = 50, width = 125, height = 25)
+            self.posNameGame.place(x = 100, y = 50, width = 126, height = 25)
             self.posPlat.bind('<ButtonRelease-1>',
                          lambda event: self.sortDisp(event, "Платформа"))
-            self.posPlat.place(x = 225, y = 50, width = 100, height = 25)
+            self.posPlat.place(x = 226, y = 50, width = 102, height = 25)
             self.posGenre.bind('<ButtonRelease-1>',
                          lambda event: self.sortDisp(event, "Жанр"))
-            self.posGenre.place(x = 325, y = 50, width = 50, height = 25)
+            self.posGenre.place(x = 328, y = 50, width = 85, height = 25)
             self.posYear.bind('<ButtonRelease-1>',
                          lambda event: self.sortDisp(event, "Год выпуска"))
-            self.posYear.place(x = 375, y = 50, width = 100, height = 25)
+            self.posYear.place(x = 413, y = 50, width = 103, height = 25)
             self.posDevel.bind('<ButtonRelease-1>',
                          lambda event: self.sortDisp(event, "Разработчик"))
-            self.posDevel.place(x = 475, y = 50, width = 100, height = 25)
+            self.posDevel.place(x = 515, y = 50, width = 159, height = 25)
             self.posPublisher.bind('<ButtonRelease-1>',
                          lambda event: self.sortDisp(event, "Издатель"))
-            self.posPublisher.place(x = 575, y = 50, width = 100, height = 25)
+            self.posPublisher.place(x = 674, y = 50, width = 159, height = 25)
             self.posPrice.bind('<ButtonRelease-1>',
                          lambda event: self.sortDisp(event, "Цена"))
-            self.posPrice.place(x = 675, y = 50, width = 50, height = 25)
+            self.posPrice.place(x = 833, y = 50, width = 44, height = 25)
             self.exit.bind('<ButtonRelease-1>')
-            self.exit.place(x = 850, y = 650, width = 75, height = 40)
+            self.exit.place(x = 1050, y = 650, width = 75, height = 40)
+            self.scrollF.place( x = 100, y = 75, width = 791, height = 500 )
             self.buttSort()
 #Поле = {"Название игры":0, "Жанр":1, "Платформа":2, "Год выпуска":3, "Цена":4, "Разработчик":5, "Издатель":6}
         def buttSort(self):
@@ -89,26 +138,49 @@ def display():
             while ( i < len(self.base)):
                 self.pNG[i].delete(0,1999)
                 self.pNG[i].insert(0,self.base[i][0])
-                self.pNG[i].place(x = 100, y = 50 + ( i + 1 ) * 25, width = 125, height = 25 )
+                self.pNG[i].grid( row = i, column = 1)
                 self.pP[i].delete(0,1999)
                 self.pP[i].insert(0,self.base[i][2])
-                self.pP[i].place(x = 225, y = 50 + ( i + 1 ) * 25, width = 100, height = 25 )
+                self.pP[i].grid( row = i, column = 2)
                 self.pG[i].delete(0,1999)
                 self.pG[i].insert(0,self.base[i][1])
-                self.pG[i].place(x = 325, y = 50 + ( i + 1 ) * 25, width = 50, height = 25 )
+                self.pG[i].grid( row = i, column = 3)
                 self.pY[i].delete(0,1999)
                 self.pY[i].insert(0,self.base[i][3])
-                self.pY[i].place(x = 375, y = 50 + ( i + 1 ) * 25, width = 100, height = 25 )
+                self.pY[i].grid( row = i, column = 4)
                 self.pD[i].delete(0,1999)
                 self.pD[i].insert(0,self.base[i][5])
-                self.pD[i].place(x = 475, y = 50 + ( i + 1 ) * 25, width = 100, height = 25 )
+                self.pD[i].grid( row = i, column = 5)
                 self.pPu[i].delete(0,1999)
                 self.pPu[i].insert(0,self.base[i][6])
-                self.pPu[i].place(x = 575, y = 50 + ( i + 1 ) * 25, width = 100, height = 25 )
+                self.pPu[i].grid( row = i, column = 6)
                 self.pPr[i].delete(0,1999)
                 self.pPr[i].insert(0,self.base[i][4])
-                self.pPr[i].place(x = 675, y = 50 + ( i + 1 ) * 25, width = 50, height = 25 )
+                self.pPr[i].grid( row = i, column = 7)
+
+                """
+                self.pP[i].place(x = 125, y = ( i + 1 ) * 25, width = 100, height = 25 )
+                self.pG[i].delete(0,1999)
+                self.pG[i].insert(0,self.base[i][1])
+                self.pG[i].place(x = 225, y = ( i + 1 ) * 25, width = 50, height = 25 )
+                self.pY[i].delete(0,1999)
+                self.pY[i].insert(0,self.base[i][3])
+                self.pY[i].place(x = 275, y = ( i + 1 ) * 25, width = 100, height = 25 )
+                self.pD[i].delete(0,1999)
+                self.pD[i].insert(0,self.base[i][5])
+                self.pD[i].place(x = 375, y = ( i + 1 ) * 25, width = 100, height = 25 )
+                self.pPu[i].delete(0,1999)
+                self.pPu[i].insert(0,self.base[i][6])
+                self.pPu[i].place(x = 475, y = ( i + 1 ) * 25, width = 100, height = 25 )
+                self.pPr[i].delete(0,1999)
+                self.pPr[i].insert(0,self.base[i][4])
+                self.pPr[i].place(x = 575, y = ( i + 1 ) * 25, width = 50, height = 25 )
+                """
                 i = i + 1
+            #self.pNG.pack(side = tk.LEFT)
+
+
+
 
         def sortDisp(self, event, newSort):
             if ( self.currSort == newSort ):
